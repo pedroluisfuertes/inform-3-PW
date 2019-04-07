cdc9c9a2ffd6f094d81d6c3971d54e67b84dd169
Pedro Luis <pedroluisfuertes@gmail.com>
2019-04-07 19:59:15 +0200

    Primer commit

---
+++ libro.php
@@ -0,0 +1,15 @@
+<?php 
+	
+	require_once './vendor/autoload.php';
+	
+	$loader = new \Twig\Loader\FilesystemLoader('.');
+
+	$twig = new \Twig\Environment($loader);
+	
+
+
+	$template = $twig -> load('./templates/html/libro.html');
+	
+	echo $template -> render();
+	
+ ?>
